01766376c0b1d93d8808e4b5102baffe7a6f9449
Hoàng Tiến Lăng <dev13@lifetek.vn>
2019-07-05 15:00:19 +0700

    firt

---
+++ seeds/client.seed.js
@@ -0,0 +1,30 @@
+const Client = require('../server/api/oauth/client.model');
+const loggger = require('../server/helpers/logger');
+
+if (process.argv.slice(2) && process.argv[2] === 'client') {
+  loggger.info('Seeding Clients...');
+  Client.findOneAndUpdate(
+    {
+      clientId: 'authServer',
+      clientSecret: 'secret',
+    },
+    {
+      grants: ['password', 'refresh_token'],
+      redirectUris: [],
+      scopes: ['grant_permissions'],
+    },
+    { upsert: true, new: true, setDefaultsOnInsert: true },
+  ).exec();
+  Client.findOneAndUpdate(
+    {
+      clientId: 'application',
+      clientSecret: 'secret',
+    },
+    {
+      grants: ['authorization_code', 'refresh_token', 'client_credentials'],
+      redirectUris: ['http://localhost:10201/oauth/callback'],
+      scopes: ['grant_permissions'],
+    },
+    { upsert: true, new: true, setDefaultsOnInsert: true },
+  ).exec();
+}
